6c496fd8249d5275ae90bc1284d9f3ba75c98a0f
TuanDao2002 <s3877347@rmit.edu.vn>
2022-12-21 17:46:31 +0700

    setup modules

--- utils/index.js
+++ utils/index.js
@@ -4,7 +4,6 @@ const sendResetPasswordEmail = require("./sendResetPasswordEmail");
 const sendVerificationEmail = require("./sendVerificationEmail");
 const createTokenUser = require("./createTokenUser");
 const attachCookiesToResponse = require("./attachCookiesToResponse");
-const verifyBlog = require("./verifyBlog");
 
 module.exports = {
     createJWT,
@@ -14,5 +13,4 @@ module.exports = {
     sendVerificationEmail,
     createTokenUser,
     attachCookiesToResponse,
-    verifyBlog
 };
