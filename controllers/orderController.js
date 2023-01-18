a944a53b70ee585761a48b5b30e75b6bfed8e7dc
TuanDao2002 <s3877347@rmit.edu.vn>
2023-01-18 09:29:23 +0700

    remove next cursor of voucher and order

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -8,200 +8,163 @@ const Voucher = require("../models/Voucher");
 const mongoose = require("mongoose");
 
 const getAllOrders = async (req, res) => {
-	let {
-		query: { isFulfilled, next_cursor },
-	} = req;
+    let {
+        query: { isFulfilled },
+    } = req;
 
-	const queryObject = {};
-	queryObject.isAvailable = true;
-	queryObject.isFulfilled = isFulfilled === "true" ? true : false;
-
-	const resultsLimitPerLoading = 10;
-	if (next_cursor) {
-		const [createdAt, _id] = Buffer.from(next_cursor, "base64")
-			.toString("ascii")
-			.split("_");
-
-		queryObject.createdAt = { $gte: createdAt };
-		queryObject._id = { $gt: _id };
-	}
-
-	let orders = Order.find(queryObject)
-		.populate({
-			path: "customer",
-			select: "-password -voucherUsed",
-		})
-		.populate({
-			path: "orderItems.item",
-			select: "-description",
-		});
-	orders = orders.sort("createdAt _id");
-	orders = orders.limit(resultsLimitPerLoading);
-	const results = await orders;
-
-	const count = await Order.countDocuments(queryObject);
-	const remainingResults = count - results.length;
-	next_cursor = null;
-	if (results.length !== count) {
-		const lastResult = results[results.length - 1];
-		next_cursor = Buffer.from(
-			lastResult.createdAt.toISOString() + "_" + lastResult._id
-		).toString("base64");
-	}
-
-	res.status(StatusCodes.OK).json({
-		results,
-		remainingResults,
-		next_cursor,
-	});
+    const queryObject = {};
+    queryObject.isAvailable = true;
+    queryObject.isFulfilled = isFulfilled === "true" ? true : false;
+    let orders = await Order.find(queryObject);
+    res.status(StatusCodes.OK).json({ orders });
 };
 
 const createOrder = async (req, res) => {
-	const {
-		user: { userId },
-		body: { orderItems, convertedPoints, voucherApplied },
-	} = req;
+    const {
+        user: { userId },
+        body: { orderItems, convertedPoints, voucherApplied },
+    } = req;
 
-	let user = await User.findOne({ _id: userId });
-	if (!user) {
-		throw new CustomError.BadRequestError("This user does not exist");
-	}
+    let user = await User.findOne({ _id: userId });
+    if (!user) {
+        throw new CustomError.BadRequestError("This user does not exist");
+    }
 
-	let subTotal = 0;
-	if (orderItems.length <= 0) {
-		throw new CustomError.BadRequestError("There is no item in this order");
-	} else {
-		for (let orderItem of orderItems) {
-			const { item, quantity } = orderItem;
-			const findItem = await Item.findOne({
-				_id: mongoose.Types.ObjectId(item),
-			});
+    let subTotal = 0;
+    if (orderItems.length <= 0) {
+        throw new CustomError.BadRequestError("There is no item in this order");
+    } else {
+        for (let orderItem of orderItems) {
+            const { item, quantity } = orderItem;
+            const findItem = await Item.findOne({
+                _id: mongoose.Types.ObjectId(item),
+            });
 
-			if (!findItem.isAvailable) {
-				throw new CustomError.BadRequestError(
-					`Item ${findItem.name} is not available`
-				);
-			}
+            if (!findItem.isAvailable) {
+                throw new CustomError.BadRequestError(
+                    `Item ${findItem.name} is not available`
+                );
+            }
 
-			if (findItem.quantity < quantity) {
-				throw new CustomError.BadRequestError(
-					`Quantity of ${findItem.name} is not enough for this order`
-				);
-			}
+            if (findItem.quantity < quantity) {
+                throw new CustomError.BadRequestError(
+                    `Quantity of ${findItem.name} is not enough for this order`
+                );
+            }
 
-			if (findItem) {
-				subTotal += findItem.price * quantity;
-			}
-		}
-	}
+            if (findItem) {
+                subTotal += findItem.price * quantity;
+            }
+        }
+    }
 
-	let discount = 0;
-	if (isNaN(convertedPoints) || convertedPoints < 0) {
-		throw new CustomError.BadRequestError(
-			"Please enter a valid positive converted points"
-		);
-	} else if (convertedPoints > user.points) {
-		throw new CustomError.BadRequestError(
-			"You do not have enough points to convert"
-		);
-	} else {
-		discount += convertedPoints * 500;
-	}
+    let discount = 0;
+    if (isNaN(convertedPoints) || convertedPoints < 0) {
+        throw new CustomError.BadRequestError(
+            "Please enter a valid positive converted points"
+        );
+    } else if (convertedPoints > user.points) {
+        throw new CustomError.BadRequestError(
+            "You do not have enough points to convert"
+        );
+    } else {
+        discount += convertedPoints * 500;
+    }
 
-	for (let voucherCode of voucherApplied) {
-		const voucher = await Voucher.findOne({
-			code: voucherCode,
-			isAvailable: true,
-		});
-		if (!voucher) {
-			throw new CustomError.BadRequestError(
-				`Voucher with code: ${voucherCode} is not available`
-			);
-		}
+    for (let voucherCode of voucherApplied) {
+        const voucher = await Voucher.findOne({
+            code: voucherCode,
+            isAvailable: true,
+        });
+        if (!voucher) {
+            throw new CustomError.BadRequestError(
+                `Voucher with code: ${voucherCode} is not available`
+            );
+        }
 
-		if (user.voucherUsed.includes(voucherCode)) {
-			throw new CustomError.BadRequestError(
-				`Voucher with code: ${voucherCode} was already used`
-			);
-		}
+        if (user.voucherUsed.includes(voucherCode)) {
+            throw new CustomError.BadRequestError(
+                `Voucher with code: ${voucherCode} was already used`
+            );
+        }
 
-		if (voucher.type === "points") {
-			discount += voucher.value * 500;
-		} else if (voucher.type === "percentage") {
-			discount += subTotal * (voucher.value / 100);
-		}
-	}
+        if (voucher.type === "points") {
+            discount += voucher.value * 500;
+        } else if (voucher.type === "percentage") {
+            discount += subTotal * (voucher.value / 100);
+        }
+    }
 
-	let total = subTotal - discount;
-	if (total < 0) {
-		total = 0;
-	}
+    let total = subTotal - discount;
+    if (total < 0) {
+        total = 0;
+    }
 
-	const newOrder = await Order.create({
-		customer: userId,
-		orderItems,
-		subTotal,
-		convertedPoints,
-		voucherApplied,
-		discount,
-		total,
-	});
-	res.status(StatusCodes.OK).json({ order: newOrder });
+    const newOrder = await Order.create({
+        customer: userId,
+        orderItems,
+        subTotal,
+        convertedPoints,
+        voucherApplied,
+        discount,
+        total,
+    });
+    res.status(StatusCodes.OK).json({ order: newOrder });
 
-	// update user's points, voucher's used and item's quantity
-	if (subTotal >= 100000) {
-		user.points += 1;
-	}
-	user.points -= convertedPoints;
-	for (let voucherCode of voucherApplied) {
-		user.voucherUsed.push(voucherCode);
-	}
-	await user.save();
+    // update user's points, voucher's used and item's quantity
+    if (subTotal >= 100000) {
+        user.points += 1;
+    }
+    user.points -= convertedPoints;
+    for (let voucherCode of voucherApplied) {
+        user.voucherUsed.push(voucherCode);
+    }
+    await user.save();
 
-	for (let orderItem of orderItems) {
-		const { item, quantity } = orderItem;
-		const findItem = await Item.findOne({
-			_id: mongoose.Types.ObjectId(item),
-		});
+    for (let orderItem of orderItems) {
+        const { item, quantity } = orderItem;
+        const findItem = await Item.findOne({
+            _id: mongoose.Types.ObjectId(item),
+        });
 
-		findItem.quantity -= quantity;
-		await findItem.save();
-	}
+        findItem.quantity -= quantity;
+        await findItem.save();
+    }
 };
 
 const deleteOrder = async (req, res) => {
-	const {
-		params: { orderId },
-	} = req;
+    const {
+        params: { orderId },
+    } = req;
 
-	const order = await Order.findOne({ _id: orderId });
-	if (!order || !order.isAvailable) {
-		throw new CustomError.BadRequestError("This order does not exist");
-	}
+    const order = await Order.findOne({ _id: orderId });
+    if (!order || !order.isAvailable) {
+        throw new CustomError.BadRequestError("This order does not exist");
+    }
 
-	order.isAvailable = false;
-	await order.save();
-	res.status(StatusCodes.OK).json({ msg: "Order is removed" });
+    order.isAvailable = false;
+    await order.save();
+    res.status(StatusCodes.OK).json({ msg: "Order is removed" });
 };
 
 const fulfillOrder = async (req, res) => {
-	const {
-		params: { orderId },
-	} = req;
+    const {
+        params: { orderId },
+    } = req;
 
-	const order = await Order.findOne({ _id: orderId });
-	if (!order || !order.isAvailable) {
-		throw new CustomError.BadRequestError("This order does not exist");
-	}
+    const order = await Order.findOne({ _id: orderId });
+    if (!order || !order.isAvailable) {
+        throw new CustomError.BadRequestError("This order does not exist");
+    }
 
-	order.isFulfilled = true;
-	await order.save();
-	res.status(StatusCodes.OK).json({ msg: "Order is fulfilled" });
+    order.isFulfilled = true;
+    await order.save();
+    res.status(StatusCodes.OK).json({ msg: "Order is fulfilled" });
 };
 
 module.exports = {
-	getAllOrders,
-	createOrder,
-	deleteOrder,
-	fulfillOrder,
+    getAllOrders,
+    createOrder,
+    deleteOrder,
+    fulfillOrder,
 };
--- controllers/voucherController.js
+++ controllers/voucherController.js
@@ -8,116 +8,83 @@ const { connectedUsers } = require("../utils");
 const notifySocket = require("../socket/notify");
 
 const getAllVouchers = async (req, res) => {
-	let {
-		query: { next_cursor },
-	} = req;
-
-	const queryObject = {};
-	queryObject.isAvailable = true;
-
-	const resultsLimitPerLoading = 10;
-	if (next_cursor) {
-		const [createdAt, _id] = Buffer.from(next_cursor, "base64")
-			.toString("ascii")
-			.split("_");
-
-		queryObject.createdAt = { $lte: createdAt };
-		queryObject._id = { $lt: _id };
-	}
-
-	let vouchers = Voucher.find(queryObject);
-	vouchers = vouchers.sort("-createdAt -_id");
-	vouchers = vouchers.limit(resultsLimitPerLoading);
-	const results = await vouchers;
-
-	const count = await Voucher.countDocuments(queryObject);
-	const remainingResults = count - results.length;
-	next_cursor = null;
-	if (results.length !== count) {
-		const lastResult = results[results.length - 1];
-		next_cursor = Buffer.from(
-			lastResult.createdAt.toISOString() + "_" + lastResult._id
-		).toString("base64");
-	}
-
-	res.status(StatusCodes.OK).json({
-		results,
-		remainingResults,
-		next_cursor,
-	});
+    const queryObject = {};
+    queryObject.isAvailable = true;
+    let vouchers = await Voucher.find(queryObject);
+    res.status(StatusCodes.OK).json({ vouchers });
 };
 
 const createVoucher = async (req, res) => {
-	const {
-		body: { code, title, description, type, value },
-	} = req;
+    const {
+        body: { code, title, description, type, value },
+    } = req;
 
-	if (!code || code === "") {
-		throw new CustomError.BadRequestError("Please provide code");
-	}
+    if (!code || code === "") {
+        throw new CustomError.BadRequestError("Please provide code");
+    }
 
-	if (!title || title === "") {
-		throw new CustomError.BadRequestError("Please provide title");
-	}
+    if (!title || title === "") {
+        throw new CustomError.BadRequestError("Please provide title");
+    }
 
-	if (!description || description === "") {
-		throw new CustomError.BadRequestError("Please provide description");
-	}
+    if (!description || description === "") {
+        throw new CustomError.BadRequestError("Please provide description");
+    }
 
-	if (!type || type === "" || !types.includes(type)) {
-		throw new CustomError.BadRequestError("Please provide valid type");
-	}
+    if (!type || type === "" || !types.includes(type)) {
+        throw new CustomError.BadRequestError("Please provide valid type");
+    }
 
-	if (!value || isNaN(value) || value <= 0) {
-		throw new CustomError.BadRequestError(
-			"Please provide valid positive value"
-		);
-	} else if (type === "percentage" && value >= 100) {
-		throw new CustomError.BadRequestError(
-			"The value of voucher should not be larger than the order's price"
-		);
-	}
+    if (!value || isNaN(value) || value <= 0) {
+        throw new CustomError.BadRequestError(
+            "Please provide valid positive value"
+        );
+    } else if (type === "percentage" && value >= 100) {
+        throw new CustomError.BadRequestError(
+            "The value of voucher should not be larger than the order's price"
+        );
+    }
 
-	const findDuplicateCode = await Voucher.findOne({
-		code: code,
-		isAvailable: true,
-	});
-	if (findDuplicateCode) {
-		throw new CustomError.BadRequestError(
-			"The code for this voucher is the same with another one"
-		);
-	}
+    const findDuplicateCode = await Voucher.findOne({
+        code: code,
+        isAvailable: true,
+    });
+    if (findDuplicateCode) {
+        throw new CustomError.BadRequestError(
+            "The code for this voucher is the same with another one"
+        );
+    }
 
-	const newVoucher = await Voucher.create({
-		code,
-		title,
-		description,
-		type,
-		value,
-	});
+    const newVoucher = await Voucher.create({
+        code,
+        title,
+        description,
+        type,
+        value,
+    });
 
-	notifySocket(req.app.io, newVoucher);
+    notifySocket(req.app.io, newVoucher);
 
-	res.status(StatusCodes.OK).json({ voucher: newVoucher });
+    res.status(StatusCodes.OK).json({ voucher: newVoucher });
 };
 
 const deleteVoucher = async (req, res) => {
-	const {
-		params: { voucherId },
-	} = req;
+    const {
+        params: { voucherId },
+    } = req;
 
-	const voucher = await Voucher.findOne({ _id: voucherId });
-	if (!voucher || !voucher.isAvailable) {
-		throw new CustomError.BadRequestError("This voucher does not exist");
-	}
+    const voucher = await Voucher.findOne({ _id: voucherId });
+    if (!voucher || !voucher.isAvailable) {
+        throw new CustomError.BadRequestError("This voucher does not exist");
+    }
 
-	voucher.isAvailable = false;
-	await voucher.save();
-	res.status(StatusCodes.OK).json({ msg: "Voucher is removed" });
+    voucher.isAvailable = false;
+    await voucher.save();
+    res.status(StatusCodes.OK).json({ msg: "Voucher is removed" });
 };
 
 module.exports = {
-	getAllVouchers,
-	createVoucher,
-	deleteVoucher,
+    getAllVouchers,
+    createVoucher,
+    deleteVoucher,
 };
